02fc1eb249e4f31059be65e58b56f18d0d064f96
Louis Atkinson <atkinsonlouis@gmail.com>
2020-11-25 21:56:48 +0200

    added tasks 17 and 18

---
+++ 18_taskMobilemenu/scripts.js
@@ -0,0 +1,8 @@
+const mobileMenu = () => {
+  const menu = document.querySelector("ul");
+
+  menu.classList.toggle("mobilemenu");
+};
+
+/* const mobileMenu = () =>
+    document.querySelector('ul').classList.toggle('mobilemenu'); */
